c3e1157b9a31c88346c05f9048894c3d6029eb8d
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-09-08 17:59:00 -0500

    CRUD BackEnd ready

--- src/main/java/co/edu/udistrital/teachers_students/Controller/TeachersStudentsApplication.java
+++ src/main/java/co/edu/udistrital/teachers_students/Controller/TeachersStudentsApplication.java
@@ -1,5 +1,6 @@
 package co.edu.udistrital.teachers_students.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,8 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import co.edu.udistrital.teachers_students.Model.Estudiante;
 import co.edu.udistrital.teachers_students.Model.Profesor;
 import co.edu.udistrital.teachers_students.Model.persistence.EstudentDAO;
+import co.edu.udistrital.teachers_students.Model.persistence.ProfesorDTO;
 import co.edu.udistrital.teachers_students.Model.persistence.TeacherDAO;
 import jakarta.annotation.PostConstruct;
+
+import java.util.ArrayList;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -46,8 +50,8 @@ public class TeachersStudentsApplication {
     }
 
     //Insertar persona
-     @PostMapping("/FrontEnd/insertarProfesor")
-    public void insertarPostulante(@RequestBody Profesor persona) {
+    @PostMapping("/FrontEnd/insertarProfesor")
+    public void insertarProfesor(@RequestBody Profesor persona) {
         try{
             gP.create(persona);
         }
@@ -56,7 +60,7 @@ public class TeachersStudentsApplication {
     }
 
     @PostMapping("/FrontEnd/insertarEstudiante")
-    public void insertarBuscador(@RequestBody Estudiante persona) {
+    public void insertarEstudiante(@RequestBody Estudiante persona) {
         try{
             gE.create(persona);
         }
@@ -64,4 +68,84 @@ public class TeachersStudentsApplication {
             System.out.println(e);
         }
     }
+
+    //Existencia de una persona
+    @GetMapping("/FrontEnd/exiteEstudiante")
+	public Boolean existeEstudiante(@RequestBody String id){
+        if (!gE.ids().isEmpty()) if(gE.ids().contains(id)) return true;
+        else return false;
+        
+        return false;
+	}
+
+    @GetMapping("/FrontEnd/exiteProfesor")
+    public Boolean existeProfesor(@RequestBody String id)throws Exception{
+        if (!gP.ids().isEmpty()) if(gP.ids().contains(id)) return true;
+        else return false;
+        
+        return false;
+	}
+
+    //Eliminar una persona
+    @PostMapping("/FrontEnd/eliminarProfesor")
+	public void eliminarProfesor(@RequestBody String cedula){
+		try {
+			gP.delete(cedula);
+		} catch (Exception e) {
+
+		}
+		System.out.println(cedula);
+	}
+
+    @PostMapping("/FrontEnd/eliminarEstudiante")
+	public void eliminarEstudiante(@RequestBody String cedula){
+		try {
+			gE.delete(cedula);
+		} catch (Exception e) {
+
+		}
+		System.out.println(cedula);
+	}
+
+    //Actualizar una persona
+    @PostMapping("/FrontEnd/editarProfesor")
+    public void editarProfesor(@RequestBody Profesor persona) {
+        try{
+            gP.update(gP.findID(persona.getId()), persona);
+        }
+        catch(Exception e){
+        }
+    }
+
+    @PostMapping("/FrontEnd/editarEstudiante")
+    public void editarEstudiante(@RequestBody Estudiante persona) {
+        try{
+            gE.update(gE.findID(persona.getId()), persona);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    //Consultas
+    //Lista de cedulas
+    @GetMapping("/FrontEnd/traerCedulasEstudiantes")
+	public ArrayList<String> traerCedulasEstudiantes()throws Exception{
+        return gE.ids();
+	}
+
+    @GetMapping("/FrontEnd/traerCedulasProfesores")
+    public ArrayList<String> traerCedulasProfesores()throws Exception{
+        return gP.ids();
+	}
+    //Persona
+    @GetMapping("/FrontEnd/traerEstudiante")
+	public Estudiante traerEstudiante(@RequestBody String id)throws Exception{
+        return gE.findID(id);
+	}
+
+    @GetMapping("/FrontEnd/traerProfesor")
+	public Profesor traerProfesor(@RequestBody String id)throws Exception{
+        return gP.findID(id);
+	}
 }
