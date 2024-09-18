2e85e6066ee72cea6f533dcd10604be263687dc9
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-09-17 22:45:05 -0500

    Version

--- backend/src/main/java/pack/controller/StudentServlet.java
+++ backend/src/main/java/pack/controller/StudentServlet.java
@@ -15,6 +15,7 @@ import pack.model.persistence.EstudianteDTO;
 @WebServlet("/api/controllerEstudiantes")
 public class StudentServlet extends HttpServlet {
 
+    private static final long serialVersionUID = 1L;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
